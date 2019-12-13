1c50bce6abe8f6311057c6af83f72114f719cb78
gravity <811203731@qq.com>
2019-12-13 15:12:00 +0800

    add

--- src/views/modules/operation/advertisement/index.vue
+++ src/views/modules/operation/advertisement/index.vue
@@ -14,8 +14,8 @@
 
       </el-form-item>
       <el-form-item>
-        <!--查询--><el-button @click="getDataList()">{{$t('manage.operation.advertisement.query')}}</el-button>
-        <!--新增--><el-button v-if="isAuth('manage:advertisement:save')" type="primary" @click="addOrUpdateHandle()">{{$t('manage.operation.advertisement.newAdd')}}</el-button>
+        <!--查询--><el-button @click="getDataList()">查询</el-button>
+        <!--新增--><el-button v-if="isAuth('manage:advertisement:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
 <!--        <el-button v-if="isAuth('manage:advertisement:delete')" type="danger" @click="deleteHandle()"-->
 <!--                   :disabled="dataListSelections.length <= 0">批量删除-->
 <!--        </el-button>-->
@@ -28,95 +28,47 @@
       @selection-change="selectionChangeHandle"
       style="width: 100%;">
 
-      <!--编号-->
-<!--      <el-table-column-->
-<!--        prop="id"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        :label="$t('manage.operation.advertisement.id')">-->
-<!--      </el-table-column>-->
-      <!--广告标题-->
-<!--      <el-table-column-->
-<!--        prop="title"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        :label="$t('manage.operation.advertisement.title')">-->
-<!--      </el-table-column>-->
+
       <!--位置--><el-table-column
-      prop="position"
+      prop="jumpMode"
       header-align="center"
       align="center"
-      :label="$t('manage.operation.advertisement.position')">
+      label="跳转方式">
       <template slot-scope="scope">
-        <!--模板跳转--><el-tag v-if="scope.row.position === 3" size="small" >{{$t('manage.operation.advertisement.position3')}}</el-tag>
-        <!--外部跳转--><el-tag v-if="scope.row.position === 2" size="small" >{{$t('manage.operation.advertisement.position2')}}</el-tag>
-        <!--详情--><el-tag v-if="scope.row.position === 1" size="small">{{$t('manage.operation.advertisement.position1')}}</el-tag>
+        <!--模板跳转--><el-tag v-if="scope.row.jumpMode === 3" size="small" >板块跳转</el-tag>
+        <!--内部跳转--><el-tag v-if="scope.row.jumpMode === 2" size="small" >内部链接</el-tag>
+        <!--外部链接--><el-tag v-if="scope.row.jumpMode === 1" size="small">外部链接</el-tag>
       </template>
     </el-table-column>
       <!--广告图片--><el-table-column
         prop="image"
         header-align="center"
         align="center"
-        :label="$t('manage.operation.advertisement.image')">
+        label="广告图片">
         <template slot-scope="scope">
           <img :src="global.showUrl+scope.row.image" alt="" style="width: 50px;height: 50px"
                v-if="scope.row.image!==null">
         </template>
       </el-table-column>
-      <!--跳转方式--><el-table-column
-        prop="jumpMode"
-        header-align="center"
-        align="center"
-        :label="$t('manage.operation.advertisement.jumpMode')">
-        <template slot-scope="scope">
-          <!--模板跳转--><el-tag v-if="scope.row.jumpMode === 3" size="small" >{{$t('manage.operation.advertisement.jumpMode3')}}</el-tag>
-          <!--外部跳转--><el-tag v-if="scope.row.jumpMode === 2" size="small" >{{$t('manage.operation.advertisement.jumpMode2')}}</el-tag>
-          <!--详情--><el-tag v-if="scope.row.jumpMode === 1" size="small">{{$t('manage.operation.advertisement.jumpMode1')}}</el-tag>
-        </template>
-      </el-table-column>
 
-<!--      <el-table-column-->
-<!--        prop="url"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        label="广告链接">-->
-<!--        <template slot-scope="scope">-->
-<!--          <el-tag size="small">{{getUrl(scope.row.url,scope.row.type,scope.row.jumpMode)}}</el-tag>-->
-<!--        </template>-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="createSysUserName"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        label="创建用户">-->
-<!--      </el-table-column>-->
-      <!--创建时间--><el-table-column
+      <!--创建时间-->
+      <el-table-column
         prop="createTime"
         header-align="center"
         align="center"
-        :label="$t('manage.operation.advertisement.createTime')">
+        label="创建时间">
       </el-table-column>
-<!--      <el-table-column-->
-<!--        prop="updateSysUserName"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        label="修改用户">-->
-<!--      </el-table-column>-->
-<!--      <el-table-column-->
-<!--        prop="updateTime"-->
-<!--        header-align="center"-->
-<!--        align="center"-->
-<!--        label="修改时间">-->
-<!--      </el-table-column>-->
-      <!--操作--><el-table-column
+
+      <!--操作-->
+      <el-table-column
         fixed="right"
         header-align="center"
         align="center"
         width="150"
-        :label="$t('manage.operation.advertisement.operation')">
+        label="操作">
         <template slot-scope="scope">
-          <!--修改--><el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{$t('manage.operation.advertisement.modify')}}</el-button>
-          <!--删除--><el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{$t('manage.operation.advertisement.delete')}}</el-button>
+          <!--修改--><el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+          <!--删除--><el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
